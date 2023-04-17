13a56a9497646197f6e39bc2d330c5323b73fea7
Tamam Ihsan <tamamihsan23@gmail.com>
2023-04-17 21:10:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Menyimpan judul halaman awal
+const title = document.title;
+
+// Menambahkan event listener ke window
+window.addEventListener('blur', () => {
+  // Ketika pengguna berpindah tab, ganti judul halaman
+  document.title = 'Kembalilah    :( ';
+});
+
+window.addEventListener('focus', () => {
+  // Ketika pengguna kembali ke tab, kembalikan judul halaman awal
+  document.title = title;
+});
+
+
+
